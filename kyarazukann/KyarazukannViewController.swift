0d3e565e329c778b2903c3dddc7122cd565b118b
Emily Nozaki <emiri3128@gmail.com>
2020-08-26 17:06:30 +0900

    のの

---
+++ kyarazukann/KyarazukannViewController.swift
@@ -0,0 +1,103 @@
+//
+//  KarazukannViewController.swift
+//  kyarazukann
+//
+//  Created by 柳和花 on 2020/06/04.
+//  Copyright © 2020 柳和花. All rights reserved.
+//
+
+import UIKit
+
+class KyarazukannViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    
+//    var saveData: UserDefaults = UserDefaults.standard
+    var number: Int! = nil
+    
+    var contentNumber: Int! = nil
+
+    var titleDeta: [String] = []
+    var saveDeta: UserDefaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UserDefaults.standard.register(defaults: ["title":titleDeta])
+       // userDefaults.register(defaults: ["title": titleDeta]
+      // ここをどーにかする!
+   
+       
+        // Do any additional setup after loading the view.
+     //   titleDeta = saveDeta.object(forKey: "title") as! [String]
+        table.dataSource = self
+        table.delegate = self
+        
+        table.reloadData()
+        
+    }
+    
+  override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    
+    //if saveDeta == nil {
+                      
+               //} else {
+                   number = (saveDeta.object(forKey: "title") as! [String]).count
+                  //}
+
+
+        if number == nil {
+            
+        } else {
+            //print(number!)
+            titleDeta = saveDeta.object(forKey: "title") as! [String]
+            //問題あり
+            print(titleDeta[number - 1])
+                      
+        
+        }
+        //print(titleDeta)
+        
+    table.reloadData()
+    }
+ 
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleDeta.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = String(titleDeta[indexPath.row])
+        
+        return cell!
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //contentNumberというInt型の変数を上の方で宣言してあげます。
+        //このcontentNumberに、タップされたcellが何行めなのか、という情報を入れておいてあげて、この変数を下の方に書いたメソッドでmemoViewControllerに引き渡すよ！
+        contentNumber = indexPath.row
+        print(contentNumber!)
+    
+    
+    
+    }
+    
+    //値を引き渡すためのメソッド
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "MemoViewController" {
+            let vc = segue.destination as! MemoViewController
+            vc.recievedContentNumber = self.contentNumber
+            //print(vc.recievedContentNumber!)
+        }
+    }
+
+    
+}
---
+++ kyarazukann/MemoViewController.swift
@@ -0,0 +1,99 @@
+//
+//  MemoViewController.swift
+//  kyarazukann
+//
+//  Created by 柳和花 on 2020/06/19.
+//  Copyright © 2020 柳和花. All rights reserved.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var table: UITableView!
+    
+//    var titleDeta: [String] = []
+    var number:Int! = nil
+    var saveDeta: UserDefaults = UserDefaults.standard
+    var titleArray: Array = [String]()
+    var array: [String] = []
+    
+    //KyarazukannViewControllerから受け取った値を入れるための変数。
+    var recievedContentNumber: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+        //titlededaのtiindexpathを表示する
+    // = saveDeta.object(forKey: "store") as! [String]
+        //ここが分からないです分からないです↑
+        table.dataSource = self
+        table.delegate = self
+        
+        table.reloadData()
+        
+        print(recievedContentNumber!)
+        
+    }
+    
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        
+        if number == nil {
+            
+        } else {
+           
+        //    titleDeta = saveDeta.object(forKey: "memo") as! [String]
+         //   print(titleDeta[number])
+         //   titleTextField.text = titleDeta[number]
+          
+           print(array[number])
+          //わからん
+            
+        }
+        
+        
+        print(titleArray)
+        
+        table.reloadData()
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+        return titleArray.count
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = String(titleArray[indexPath.row])
+        
+        return cell!
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // セルの選択を解除
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // 別の画面に遷移
+        performSegue(withIdentifier: "toNextViewController", sender: nil)
+    }
+    
+    
+    
+    
+    
+}
+
